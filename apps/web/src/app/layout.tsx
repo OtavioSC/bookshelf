96f77e7adb39293600ef3de0cd9e1f8f6cb71809
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-09-02 13:07:47 -0300

    feat: add login page and new background color to layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "./styles.css";
+import { Roboto_Slab } from "next/font/google";
+
+const roboto = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400", "600", "800"],
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>{children}</body>
     </html>
   );
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,24 +1,9 @@
-import { log } from "logger";
-import { CounterButton, NewTabLink } from "ui";
+import "./page.css";
 
-export const metadata = {
-  title: "Store | Kitchen Sink",
-};
-
-export default function Store() {
-  log("Hey! This is Home.");
+export default function Page() {
   return (
     <div className="container">
-      <h1 className="title">
-        Store <br />
-        <span>Kitchen Sink</span>
-      </h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> +{" "}
-        <NewTabLink href="https://nextjs.org/">Next.js</NewTabLink>
-      </p>
+      <h1 className="title">Bookshelf</h1>
     </div>
   );
 }
