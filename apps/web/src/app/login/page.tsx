139edcefd5fb138b9e720bc1308d04ddf10080f7
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-09-02 16:04:24 -0300

    feat: add login form components

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,7 +1,25 @@
+import { AlertTitle, TextField, Button } from "@mui/material";
+import Image from "next/image";
+import Background from "../../../public/library.jpg";
+import "./page.css"
+
 export default function Login() {
   return (
-    <div className="container">
-      <h1 className="title">Login</h1>
+    <div className="login-container">
+      <div className="login-content">
+        <AlertTitle className="title">Login</AlertTitle>
+        <form className="login-form">
+          <TextField
+            className="login-input"
+            id="filled-basic" label="Email" variant="filled" type="email" />
+          <TextField
+            className="login-input"
+            id="filled-basic" label="Password" variant="filled" type="password" />
+          <Button variant="contained" color="success">Submit</Button>
+        </form>
+      </div>
+      <div className="login-background">
+      </div>
     </div>
   );
 }
