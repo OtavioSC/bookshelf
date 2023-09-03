5db2ecb446faa0f64b7ce59dcc20b1447d3d9d44
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-09-03 12:54:32 -0300

    feat: add database connection in index.ts file

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,15 @@
 import { createServer } from "./server";
 import { log } from "logger";
+import createDatabase from "./database/database";
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
   log(`api running on ${port}`);
+  try {
+    createDatabase();
+  } catch (err) {
+    log(err);
+  }
 });
