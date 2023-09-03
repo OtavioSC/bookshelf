f6c7933dd408c400340e75de08b64132f3915bb0
Otávio Carmo <54047040+OtavioSC@users.noreply.github.com>
2023-09-03 11:32:51 -0300

    feature: login screen design (#1)
    * chore: install material-ui and emotion in web app
    
    * chore: update pnpm-lock
    
    * feat: add login form components
    
    * feat: add login background image
    
    * feat: add library background image
    
    * feat: add forgot password button and create account button
    
    * chore: update pnpm-lock
    
    * feat: improving form design colors

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,7 +1,40 @@
+import { TextField, Button } from "@mui/material";
+import "./page.css";
+
 export default function Login() {
   return (
-    <div className="container">
-      <h1 className="title">Login</h1>
-    </div>
+    <main className="login-container">
+      <section className="login-content">
+        <h1 className="title">Login to your Account</h1>
+        <form className="login-form">
+          <TextField
+            className="login-input"
+            id="filled-basic"
+            label="Email"
+            variant="filled"
+            type="email"
+          />
+          <TextField
+            className="login-input"
+            id="filled-basic"
+            label="Password"
+            variant="filled"
+            type="password"
+          />
+          <span className="login-forgot">
+            <a href="/forgot">Forgot Password?</a>
+          </span>
+          <Button variant="contained" color="secondary">
+            Submit
+          </Button>
+        </form>
+        <div className="login-details">
+          <span className="login-register">
+            New here? <a href="/register">Join us!</a>
+          </span>
+        </div>
+      </section>
+      <aside className="login-background"></aside>
+    </main>
   );
 }
