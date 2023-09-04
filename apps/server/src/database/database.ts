39c45eb0e353275f4f6e98a83e7fa5d73f7479a4
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-09-04 14:18:58 -0300

    ref: rewrite database connection string

--- apps/server/src/database/database.ts
+++ apps/server/src/database/database.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
+const MONGO_URI: string = process.env.MONGO_URI || "";
+
 export default async function createDatabase() {
-  const db = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const db = await mongoose.connect(MONGO_URI);
   return db;
 }
